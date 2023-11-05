4709df4a3725829d2e399d1744bd9df4962fec15
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2023-11-05 06:23:05 +0330

    Remove extra codes and create layout and absolute route

---
+++ src/containers/layout/header.js
@@ -0,0 +1,13 @@
+
+const Header = () => {
+  return (
+    <header className="2xl:container mx-auto  flex flex-col  min-[550px]:flex-row min-[550px]:justify-between justify-center items-center w-full mb-4 p-4 min-[550px]:py-6 bg-blue-600 rounded-b-lg">
+      <h1 className="font-bold text-slate-700 text-3xl mb-4 min-[550px]:mb-0 lg:text-4xl">
+        Next App
+      </h1>
+     
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Layout from "@containers/layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <div className="bg-red-600">
+        <h1>welcome to the app</h1>
+      </div>
+    </Layout>
+  );
+}
