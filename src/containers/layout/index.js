3c803b545410bb4cb3f3bac50d013f2a73f1f775
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2023-11-06 01:59:52 -0600

    add option get data blogs From backend and use it and add option navigate to the page for blogs

--- src/containers/layout/index.js
+++ src/containers/layout/index.js
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import Footer from "./footer";
 import Header from "./header";
-
+import { animateScroll as scroll } from 'react-scroll';
 const Layout = ({ children }) => {
+  useEffect(()=>{
+    scroll.scrollToTop()
+  },[])
   return (
     <main className=" min-h-screen flex flex-col items-center justify-start bg-slate-100">
       <Header />
